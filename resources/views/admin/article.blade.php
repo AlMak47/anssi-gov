d9bb9c7928f9fe0919ec330b221d0cc138413922
AlMak47 <almamy@smartechguinee.com>
2019-09-30 16:46:27 +0000

    reajustement ajoute d'outil de mise en forme lors de l'ajout darticle

--- resources/views/admin/article.blade.php
+++ resources/views/admin/article.blade.php
@@ -71,7 +71,25 @@ ADMIN-ARTICLE
 
 var quill = new Quill('#editor-container', {
   modules: {
-    toolbar: true
+    toolbar: [
+			['bold', 'italic', 'underline', 'strike','link'],        // toggled buttons
+		  ['blockquote', 'code-block','image'],
+
+		  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+		  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+		  [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+		  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+		  [{ 'direction': 'rtl' }],                         // text direction
+
+		  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+		  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+		  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+		  [{ 'font': [] }],
+		  [{ 'align': [] }],
+
+		  ['clean']
+		]
   },
   placeholder: 'Redigez ici ...',
   theme: 'snow'
